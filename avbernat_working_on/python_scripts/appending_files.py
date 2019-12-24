5550931d18b171d14aee3a451436a1e505986f98
Anastasia Bernat <anastasiabernat@Anastasias-MacBook-Pro.local>
2019-12-24 17:43:20 -0600

    python_scripts

--- avbernat_working_on/python_scripts/appending_files.py
+++ avbernat_working_on/python_scripts/appending_files.py
@@ -40,9 +40,15 @@ from decimal import Decimal
 # When files have varying number of columns:
 
 # Paths
-filepath1 = r"/Users/anastasiabernat/Desktop/event_markers2/set74-11-07-19-A-events.txt"
-filepath2 = r"/Users/anastasiabernat/Desktop/event_markers2/set74-11-07-19-A2-events.txt"
-outpath = r"/Users/anastasiabernat/Desktop/event_markers_combined/set74-11-07-19-A.txt"
+path1 = r"/Users/anastasiabernat/Desktop/event_markers2/"
+path2 = r"/Users/anastasiabernat/Desktop/event_markers2/"
+filename1 = "set73-11-06-19-A-events.txt"
+filename2 = "set73-11-06-19-A2-events.txt"
+filepath1 = path1 + filename1
+filepath2 = path2 + filename2
+outpath = r"/Users/anastasiabernat/Desktop/event_markers_combined/set73-11-06-19-A.txt"
+
+print("Merging " + filename1 + " with " + filename2)
 
 # Converting data to DataFrame object
 header = ["time","1","2","3","4",
@@ -56,6 +62,50 @@ df1 = pd.DataFrame(data1)
 data2 = pd.read_csv(filepath2, names=header, sep=",", header=None, dtype=d)
 df2 = pd.DataFrame(data2)
 
+# Resetting the first row, fith column equal to 0 (because a new bug did not actually come on here)
+if df1.iat[0,5] != 0:
+    df1.iat[0,5] = 0
+if df2.iat[0,5] != 0:
+    df2.iat[0,5] = 0
+    
+# Checking and reformating 'event_marker' strings to make them all uniform
+filtered_df1 = df1[df1['event_marker'].notnull()]
+filtered_df2 = df2[df2['event_marker'].notnull()]
+
+event_name1 = filtered_df1['event_marker']
+event_name2 = filtered_df2['event_marker']
+
+for string in event_name1.iteritems():
+    row = string[0]
+    column = 10
+    string = string[1]
+    integer = [int(s) for s in string.split() if s.isdigit()]
+    integer = str(integer[0])
+    if string.startswith(integer):
+        print('ID appears first: ' + string)
+    else:
+        print('ID does not appear first: ' + string)
+        print('This is its row: ' + str(row))
+        new_string = integer + " is on"
+        df1.iat[row, column] = new_string
+        print('ID was this: ' + string + ' --> ID now appears first: ' + new_string)
+
+
+for string in event_name2.iteritems():
+    row = string[0]
+    column = 10
+    string = string[1]
+    integer = [int(s) for s in string.split() if s.isdigit()]
+    integer = str(integer[0])
+    if string.startswith(integer):
+        print('ID appears first: ' + string)
+    else:
+        print('ID does not appear first: ' + string)
+        print('This is its row: ' + str(row))
+        new_string = integer + " is on"
+        df1.iat[row, column] = new_string
+        print('ID was this: ' + string + ' --> ID now appears first: ' + new_string)
+
 # Accruing time
 time_passed = df1["time"].iloc[-1]
 df2["time"]= df2["time"].apply(lambda x: x + time_passed + .01)
@@ -63,14 +113,66 @@ df2["time"]= df2["time"].apply(lambda x: x + time_passed + .01)
 # Appending the files
 df3 = df1.append(df2)
 
-# Saving to csv file
+ Saving to csv file
 
 df3.to_csv(outpath, header=None, index=None, sep=',', mode='w', float_format='%1.5f')
 
 
 #******************************************************************************
 
-# EXTRA
+### Converting and reformating multiple-hour long files
+##
+### Paths
+##path1 = r"/Users/anastasiabernat/Desktop/event_markers2/"
+##filename1 = "set76-11-08-19-B-events.txt"
+##filepath1 = path1 + filename1
+##outpath = r"/Users/anastasiabernat/Desktop/event_markers_combined/set76-11-08-19-B.txt"
+##
+##print("Reformating: " + filename1)
+##
+### Converting data to DataFrame object
+##header = ["time","1","2","3","4",
+##          "event_happened","event_num","zero","event_date","event_time","event_marker"]
+##d = {"time": float, "1": float, "2": float, "3": float, "4": float,
+##     "event_happened": float, "event_num": float, "zero": float,
+##     "event_date": str, "event_time": str ,"event_marker": str}
+##data1 = pd.read_csv(filepath1, names=header, sep=",", header=None, dtype=d)
+##df1 = pd.DataFrame(data1)
+##
+##
+### Resetting the first row, fith column equal to 0 (because a new bug did not actually come on here)
+##if df1.iat[0,5] != 0:
+##    df1.iat[0,5] = 0
+##
+### Checking and reformating 'event_marker' strings to make them all uniform
+##
+##filtered_df1 = df1[df1['event_marker'].notnull()]
+##
+##event_name1 = filtered_df1['event_marker']
+##
+##for string in event_name1.iteritems():
+##    row = string[0]
+##    column = 10
+##    string = string[1]
+##    integer = [int(s) for s in string.split() if s.isdigit()]
+##    integer = str(integer[0])
+##    if string.startswith(integer):
+##        print('ID appears first: ' + string)
+##    else:
+##        print('ID does not appear first: ' + string)
+##        print('This is its row: ' + str(row))
+##        new_string = integer + " is on"
+##        df1.iat[row, column] = new_string
+##        print('ID was this: ' + string + ' --> ID now appears first: ' + new_string)
+##
+### Saving to csv file
+##
+##df1.to_csv(outpath, header=None, index=None, sep=',', mode='w', float_format='%1.5f')
+
+
+#******************************************************************************
+
+# EXTRA code testing out
     
 #np.savetxt(outpath, df3.values, fmt='%1.5E')
 
@@ -86,4 +188,8 @@ df3.to_csv(outpath, header=None, index=None, sep=',', mode='w', float_format='%1
 ##df3[0].apply(lambda x: '%1.2f' % x)
 ##df3_channels.reset_index(drop=True).style.format('%1.5E')
 ##df3_channels.set_option.display.float_format ='%1.5E'.format
+
+#******************************************************************************
+
+    
 
--- avbernat_working_on/python_scripts/splitting_files.py
+++ avbernat_working_on/python_scripts/splitting_files.py
@@ -3,13 +3,20 @@ import os
 import pandas as pd
 import numpy as np
 
-#******************************************************************************
+#************************************************************************************
 # Splitting flight trial txt files based on event markers.
-#******************************************************************************
+#************************************************************************************
 
+#************************************************************************************
 # Creating Dictionaries
+# Input: Data with the minimum following columns: ID, chamber, set_number, and died?
+# Output: Two dictionaries that have the set_number, channel_letter, and channel_num
+#           as keys and ID as values. The first_flight_dict is used for the first 4
+#           bugs that enter the chamber. The current_flight_dict holds the remaining 
+#           bugs that get swapped in and out of the chambers.
+#************************************************************************************
 
-datapath = r"/Users/anastasiabernat/Desktop/dispersal_data/mutliple_hours_data2.csv"
+datapath = r"/Users/anastasiabernat/Desktop/dispersal_data/multiple_hours/multiple_hours_data2.csv"
 
 first_flight_dict = {}
 current_flight_dict = {} 
@@ -37,11 +44,16 @@ with open(datapath, "r") as data_file:
             print('PROBLEM, BUG %s SHOWS UP AGAIN'%ID)
 
 #print(first_flight_dict)
-#print(current_flight_dict) 
+#print(current_flight_dict)
 
+#************************************************************************************
 # Finding Event Markers and Creating Dictionary Rows
+# Input: Txt file with the time, voltage readings, and event marker information.
+# Output: A newly formatted file with 4 new rows of IDs that mapped out when bugs 
+#           come in and to which channel.
+#************************************************************************************
 
-path = r"/Users/anastasiabernat/Desktop/event_markers2/Test/test_small/one_event/best_sample/full/"
+path = r"/Users/anastasiabernat/Desktop/event_markers_combined/"
 dir_list = sorted(os.listdir(path))
 
 header = ["time","1","2","3","4",
@@ -56,6 +68,7 @@ for file in dir_list:
     before_first_event = True
     set_num = file.split('-')[0][3:]
     channel_letter = file.split('.')[0][-1]
+    print(file + "--------------------------------")
     
     with open(filepath, 'r', encoding='latin') as input_file:
         reader = csv.DictReader(input_file, delimiter = ',', fieldnames=header)
@@ -77,7 +90,10 @@ for file in dir_list:
                                 
                 before_first_event = False
                 
-            elif (not before_first_event) and (row['event_happened'] != '0'):
+            #elif (not before_first_event) and (row['event_happened'] != '0' or row['event_happened'] != '0.00000'):
+            elif (not before_first_event) and (int(float(row['event_happened'])) != 0):
+                if row["event_marker"] == '':
+                    continue
                 new_bug = int(row['event_marker'].split(' ')[0])
                 new_channel = current_flight_dict[(set_num, channel_letter, new_bug)]
                 print('new bug %s replacing old bug %s at channel %s'%(new_bug,
@@ -92,15 +108,21 @@ for file in dir_list:
             new_row['channel4_bug'] = current_bugs['channel4']
 
             full_data.append(new_row)
-
-    with open(r"/Users/anastasiabernat/Desktop/files_to_split/output_test.txt","w") as output_file:
+            
+    with open(r"/Users/anastasiabernat/Desktop/files_to_split2/" + file,"w") as output_file:
         writer = csv.DictWriter(output_file, delimiter=',', fieldnames=new_row.keys())
         for r in full_data:
             writer.writerow(r)
-            
-# Splitting files
 
-filepath = r"/Users/anastasiabernat/Desktop/files_to_split/"
+#************************************************************************************
+# Splitting files
+# Input: The newly formatted file with 4 new rows of IDs.
+# Output: New txt files: the filename contains the bug ID at the end and the file 
+#       contains the time and voltage readings specific to when the bug entered
+#       and left.
+#************************************************************************************          
+
+filepath = r"/Users/anastasiabernat/Desktop/files_to_split2/"
 dir_files = sorted(os.listdir(filepath))
 col_names = ["time","1","2","3","4",
           "event_happened","chn1_ID","chn2_ID","chn3_ID","chn4_ID"]
@@ -109,6 +131,7 @@ for f in dir_files:
     if f.startswith("."):
         continue
     filename = filepath + str(f)
+    print("File splitting: " + f + "--------------------------------")
     for channel in range(1,5):
         ID_data = {}
         with open(filename,"r") as input_file:
@@ -123,7 +146,7 @@ for f in dir_files:
                 ID_data[ID].append(new_row)
 
         for key_ID, data in ID_data.items():
-            with open(r"/Users/anastasiabernat/Desktop/split_files/" +
+            with open(r"/Users/anastasiabernat/Desktop/split_files2/" +
                       os.path.basename(filename).split(".")[0] + str(channel) + '_' + str(key_ID) + ".txt","w") as output_file:
                 writer = csv.DictWriter(output_file, fieldnames=new_row.keys())
                 for r in data:
