8b46419dbee451248cfcdd59aac4429a02a8e778
Meredith Cenzer <mlcenzer@ucdavis.edu>
2020-03-03 15:46:07 -0600

    updated flight distance analyses to consider the multi-hour fliers; tried B vs. C, with little success in longest flying class. Leaving these alone for the time being.

--- Meredith is working on/stats/flight_distance.R
+++ Meredith is working on/stats/flight_distance.R
@@ -38,7 +38,7 @@ for(row in 1:length(data_all$days_from_start)){
 
 data_all$min_from_start_c<-data_all$min_from_start-mean(data_all$min_from_start)
 
-data_flew<-data_all[data_all$distance!=0,]
+data_flew<-data_all[data_all$distance!=0 & data_all$flew_b==1,]
 
 
 
@@ -230,10 +230,44 @@ plot(summary90$distance~summary90$sym_dist, pch=c(19,22)[as.factor(summary90$hos
 
 
 #####Keep ID number in here
+######Distance will not converge, or shows nothing interesting, within this subset. Look here at flight type instead...
 data_long<-data_flew[data_flew$trial_type=="Tlong",]
 
+data_long$flight_type_b<-1
+data_long$flight_type_b[data_long$flight_type=="B"]<-0
+
+
 #######No chamber effects
-summary(glmer(distance~chamber + (1|ID), data=data_long, family=Gamma(link="log")))
+summary(glmer(flight_type_b~chamber + (1|ID), data=data_long, family=binomial))
+
+#######No effect of test date
+summary(glmer(flight_type_b~days_from_start + (1|ID), data=data_long, family=binomial))
+
+#######Marginal effect of test time: later starting bugs don't get as long to fly
+summary(glmer(flight_type_b~min_from_start_c + (1|ID), data=data_long, family=binomial))
+
+
+data<-data.frame(R=data_long$flight_type_b, A=data_long$host_c, B=data_long$sex_c, C=data_long$sym_dist, D=data_long$min_from_start_c, X=data_long$ID) ##Won't converge with D, drop for now
+
+library(lme4)
+
+#run AICprobs script
+setwd("/Users/Meredith/Desktop/Documents/R/generic models/")
+source("AICprobabilities.R")
+source("generic models-binomial glmer 3-FF.R")
+sort(summary$AIC)
+sort(P, decreasing=TRUE, index.return=TRUE)
+###Not converging: m25, m28, m31, m32, m34
+###Really nothing here either.
+
+
+
+
+
+
+
+#######No chamber effects
+summary(glmer(distance~chamber + (1|ID), data=data_long1, family=Gamma(link="log")))
 
 #######No effect of test date
 summary(glmer(distance~days_from_start + (1|ID), data=data_long, family=Gamma(link="log")))
@@ -241,9 +275,12 @@ summary(glmer(distance~days_from_start + (1|ID), data=data_long, family=Gamma(li
 #######YES effect of test time: later starting bugs don't get as long to fly
 summary(glm(distance~min_from_start_c, data=data_long, family=Gamma(link="log")))
 
+summarylong<-aggregate(distance~ID, FUN=mean, data=data_long)
+summarylong$n<-aggregate(distance~ID, FUN=length, data=data_long)$distance
+summarylong
 
 
-data<-data.frame(R=data_long$distance, A=data_long$host_c, B=data_long$sex_c, C=data_long$lat_c, D=data_long$min_from_start_c, X=data_long$ID)
+data<-data.frame(R=data_long$distance, A=data_long$host_c, B=data_long$sex_c, C=data_long$sym_dist, D=data_long$min_from_start_c, X=data_long$ID)
 
 library(lme4)
 
@@ -253,3 +290,113 @@ source("AICprobabilities.R")
 source("generic models-Gamma glmer 4-FF log link.R")
 sort(summary$AIC)
 sort(P, decreasing=TRUE, index.return=TRUE)
+
+########Serious issues of non-convergence when attempting to include ID as a random factor. Try, for now, looking only at the first measurement for each individual. 
+###This ended up being unhelpful.
+data_long1<-data_long[1,]
+for(row in 1:length(data_long$ID)){
+	new_row<-length(data_long1$ID)+1
+	if(data_long$ID[row] %in% data_long1$ID) print("Hooray!")
+	else{
+		data_long1[new_row,]<-data_long[row,]
+	}
+}
+data<-data.frame(R=data_long1$distance, A=data_long1$host_c, B=data_long1$sex_c, C=data_long1$sym_dist, D=data_long1$min_from_start_c)
+
+library(lme4)
+
+#run AICprobs script
+setwd("/Users/Meredith/Desktop/Documents/R/generic models/")
+source("AICprobabilities.R")
+source("generic models-Gamma glm 4-FF log link.R")
+sort(summary$AIC)
+sort(P, decreasing=TRUE, index.return=TRUE)
+
+
+
+
+###Try subsetting by those that spent less than 30 minutes on the mill vs. those that did not; this is actually not possible with the current data format, so split by burst vs. not (which is essentially the same):
+
+data_cont<-data_long[data_long$flight_type!="B",]
+
+summarycont<-aggregate(distance~ID, FUN=mean, data=data_cont)
+summarycont$n<-aggregate(distance~ID, FUN=length, data=data_cont)$distance
+summarycont
+#######No continuous fliers were measured more than once. Hmmm....
+
+#######No chamber effects
+summary(glm(distance~chamber, data=data_cont, family=Gamma(link="log")))
+
+#######No effect of test date
+summary(glm(distance~days_from_start, data=data_cont, family=Gamma(link="log")))
+
+#######No effect of start time
+summary(glm(distance~min_from_start_c, data=data_cont, family=Gamma(link="log")))
+
+data<-data.frame(R=data_cont$distance, A=data_cont$host_c, B=data_cont$sex_c, C=data_cont$sym_dist)
+
+library(lme4)
+
+#run AICprobs script
+setwd("/Users/Meredith/Desktop/Documents/R/generic models/")
+source("AICprobabilities.R")
+source("generic models-Gamma glm 3-FF inverse link.R")
+sort(summary$AIC)
+sort(P, decreasing=TRUE, index.return=TRUE)
+#Top  model is m0
+
+###Within this subset, there is no effect of anything. But also, there is very little power to detect anything, given the limited number of bugs that made it to this point.
+
+summarycont<-aggregate(distance~sex, data=data_cont, FUN=mean)
+summarycont$n<-aggregate(distance~sex, FUN=length, data=data_cont)$distance
+summarycont
+
+
+
+
+
+data_burst<-data_long[data_long$flight_type=="B",]
+
+summaryburst<-aggregate(distance~ID, FUN=mean, data=data_burst)
+summaryburst$n<-aggregate(distance~ID, FUN=length, data=data_burst)$distance
+summaryburst
+
+#######No chamber effects
+summary(glm(distance~chamber, data=data_burst, family=Gamma(link="log")))
+
+#######Appears to be an effect of test date, but only when accounting for ID
+summary(glm(distance~days_from_start, data=data_burst, family=Gamma(link="log")))
+
+#######Appears to be an effect of start time, but unable to run this while controlling for ID
+summary(glm(distance~min_from_start_c, data=data_burst, family=Gamma(link="log")))
+
+###Take a look:
+summary_min<-aggregate(distance~min_from_start_c, data=data_burst, FUN=mean)
+summary_min
+
+####I suspect both of these effects are real. However, we do not have the power to include them in the overall model structure; because our factors of interest were randomized with respect to these, they should not alter our predictions for effect size. But, we should be aware of them.
+
+data<-data.frame(R=data_burst$distance, A=data_burst$host_c, B=data_burst$sex_c, C=data_burst$sym_dist, X=data_burst$ID)
+
+library(lme4)
+
+#run AICprobs script
+setwd("/Users/Meredith/Desktop/Documents/R/generic models/")
+source("AICprobabilities.R")
+source("generic models-Gamma glmer 3-FF log link.R")
+sort(summary$AIC)
+sort(P, decreasing=TRUE, index.return=TRUE)
+##Except for m20, m30, and m32, any model containing the random effect of individual did not work. Which is sadly not okay.
+
+
+
+
+
+
+
+##########If it still won't converge, time to bust out the big (Bayesian) guns? 
+##Return here.
+
+
+
+
