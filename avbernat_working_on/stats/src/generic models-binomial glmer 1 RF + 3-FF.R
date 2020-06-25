e54dc0836e0e1359465539a26f03c399a080c256
Anastasia Bernat <anastasiabernat@Anastasias-MacBook-Pro.local>
2020-06-25 13:09:08 -0500

    morphology modeling

---
+++ avbernat_working_on/stats/src/generic models-binomial glmer 1 RF + 3-FF.R
@@ -0,0 +1,41 @@
+
+#generic three factor analysis with binomial data
+
+m0<-glmer(R~1 + (1|X), family=binomial, data=data)
+m1<-glmer(R~A + (1|X), family=binomial, data=data)
+m2<-glmer(R~B + (1|X), family=binomial, data=data)
+m3<-glmer(R~C + (1|X), family=binomial, data=data)
+
+m4<-glmer(R~A+B + (1|X), family=binomial, data=data)
+m5<-glmer(R~A+C + (1|X), family=binomial, data=data)
+m6<-glmer(R~B+C + (1|X), family=binomial, data=data)
+m7<-glmer(R~A+B+C + (1|X), family=binomial, data=data)
+
+m8<-glmer(R~A*B + (1|X), family=binomial, data=data)
+m9<-glmer(R~A*C + (1|X), family=binomial, data=data)
+m10<-glmer(R~B*C + (1|X), family=binomial, data=data)
+m11<-glmer(R~A*B + C + (1|X), family=binomial, data=data)
+m12<-glmer(R~A*C + B + (1|X), family=binomial, data=data)
+
+m13<-glmer(R~B*C + A + (1|X), family=binomial, data=data)
+m14<-glmer(R~A*B + A*C + (1|X), family=binomial, data=data)
+m15<-glmer(R~A*B + B*C + (1|X), family=binomial, data=data)
+m16<-glmer(R~A*C + B*C + (1|X), family=binomial, data=data)
+
+m17<-glmer(R~A*B + A*C + B*C + (1|X), family=binomial, data=data)
+
+m18<-glmer(R~A*B*C + (1|X), family=binomial, data=data)
+
+
+#identify top models using AIC
+summary<-AIC(m1,m2,m3,m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, 
+             m17, m18, m0)
+
+sort(summary$AIC, index.return=TRUE)
+
+
+#Run AICprobabilities in generic models folder
+
+P<-AICprobs(summary$AIC)
+sort(P, index.return=TRUE, decreasing=TRUE) #remember, we want the largest one of these
+library(lmtest) #you might want this later to test for heteroscedacity in your models
