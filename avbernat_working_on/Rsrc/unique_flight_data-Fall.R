7da7106ccc5b58d02fff4e56ad63077f03e214f2
Anastasia Bernat <anastasiabernat@Anastasias-MacBook-Pro.local>
2021-02-26 17:26:35 -0600

    created Table 1

--- avbernat_working_on/Rsrc/unique_flight_data-Fall.R
+++ avbernat_working_on/Rsrc/unique_flight_data-Fall.R
@@ -4,6 +4,15 @@ create_delta_data.Fall <- function(data) {
     group_by(ID, sex, population, site, host_plant, latitude, longitude, 
              host_c, sex_c, w_morph_c, lat_c, sym_dist) %>%
     summarise_all(funs(list(na.omit(.))))
+ 
+  # Filter out bugs that were not tested at least twice:
+  rows_remove <- c()
+  for (row in 1:nrow(d)){
+    if (length(d$flew[[row]]) < 2) {
+      rows_remove <- c(rows_remove, row)
+    }
+  }
+  d <- d[-rows_remove, ]
   
   d$num_flew <- 0
   d$num_notflew <- 0
@@ -19,7 +28,15 @@ create_delta_data.Fall <- function(data) {
   d$num_flew_twice <- 0
   d$num_egg_twice <- 0
   
-  for(row in 1:length(d$flew_b)){
+  d$mass_diff2 <- 0
+  d$mass_per2 <- 0
+  d$flew_diff2 <- 0
+  d$egg_diff2 <- 0
+  d$num_flew_twice2 <- 0
+  d$num_egg_twice2 <- 0
+  
+  # compare 1st and 2nd trial
+  for(row in 1:nrow(d)){
     
     n_flew <- sum(d$flew_b[[row]] == 1) # total number of times did fly among trails
     d$num_flew[[row]] <- n_flew 
@@ -44,18 +61,18 @@ create_delta_data.Fall <- function(data) {
     d$speed_diff[[row]] <- d$average_speed[[row]][2] - d$average_speed[[row]][1]  # T2 - T1
     d$egg_diff[[row]] <- d$eggs_b[[row]][2] - d$eggs_b[[row]][1]  # T2 - T1
     
+    if (length(d$flew_b[[row]]) > 2) {
+      d$mass_diff2[[row]] <- d$mass[[row]][3] - d$mass[[row]][2]
+      d$mass_per2[[row]] <- (d$mass_diff2[[row]] / d$mass[[row]][2]) * 100
+      d$flew_diff2[[row]] <- d$flew_b[[row]][3] - d$flew_b[[row]][2]
+      d$egg_diff2[[row]] <- d$eggs_b[[row]][3] - d$eggs_b[[row]][2] 
+      
+      d$num_flew_twice2[[row]] <- d$flew_b[[row]][3] + d$flew_b[[row]][2]
+      d$num_egg_twice2[[row]] <- d$eggs_b[[row]][3] + d$eggs_b[[row]][2]
+    }
   }
   
   d <- select(d, -filename, -channel_letter, -set_number)
-  
-  # Filter out bugs that were not tested twice:
-  rows_remove <- c()
-  for (row in 1:nrow(d)){
-    if (length(d$flew[[row]]) < 2) {
-      rows_remove <- c(rows_remove, row)
-    }
-  }
-  d <- d[-rows_remove, ]
   
   # for flight case building
   d$flight_case = NA
@@ -65,12 +82,25 @@ create_delta_data.Fall <- function(data) {
   d$flight_case[d$flew_diff == 1] = 1
   d$flight_case = as.factor(d$flight_case)
   
+  d$flight_case2 = NA
+  d$flight_case2[d$num_flew_twice2 == 0] = 0
+  d$flight_case2[d$num_flew_twice2 == 2] = 2
+  d$flight_case2[d$flew_diff2 == -1] = -1
+  d$flight_case2[d$flew_diff2 == 1] = 1
+  d$flight_case2 = as.factor(d$flight_case2)
+  
   # for egg case building
   d$egg_case = NA
   d$egg_case[d$num_egg_twice == 0] = 0
   d$egg_case[d$num_egg_twice == 2] = 2
   d$egg_case[d$egg_diff == -1] = -1
   d$egg_case[d$egg_diff == 1] = 1
+  
+  d$egg_case2 = NA
+  d$egg_case2[d$num_egg_twice2 == 0] = 0
+  d$egg_case2[d$num_egg_twice2 == 2] = 2
+  d$egg_case2[d$egg_diff2 == -1] = -1
+  d$egg_case2[d$egg_diff2 == 1] = 1
   
   return(d)
 }
