6909c599d568c713cfbc8799a6d14d49e6b83676
mlcenzer <32850195+mlcenzer@users.noreply.github.com>
2021-07-16 17:03:39 -0500

    added effect size calculations and updated x/y label names in multinom functions; did not successfully get effect sizes to plot with coloration still determined by p, but shouldn't be breaking anything.

--- avbernat_working_on/Rsrc/multinom_functions.R
+++ avbernat_working_on/Rsrc/multinom_functions.R
@@ -121,9 +121,10 @@ prediction_equations2 = function(tb, var1, var2, third_eq = TRUE) {
 
 #### Determine which multinomial model equations are significant.
 
-get_significant_models = function(val_num){
+get_significant_models = function(val_num, effect_cat=1){
   baselines = -1:2
   pvalues = matrix(NA, nrow = 4, ncol = 4)
+  effects = matrix(NA, nrow = 4, ncol = 4)
   r = 0
   model_list = list()
   for (i in baselines) {
@@ -138,6 +139,7 @@ get_significant_models = function(val_num){
       c = c + 1
       e = as.integer(e) + 2 
       pvalues[r,e] = model_table[c,val_num]
+      effects[r,e] = model_table[c, effect_cat]
     } 
   }
   colnames(pvalues) = baselines
@@ -145,17 +147,18 @@ get_significant_models = function(val_num){
   
   #plot
   par(mar=c(5.1, 4.1, 3.1, 4.1))
-  plot(pvalues, ylab="Baselines", xlab="Equation", main="p-values", 
+  plot(effects, ylab="Baselines", xlab="Equation", main="p-values", 
        na.col="white", col=rev(grey.colors(7)), digits=4, 
        breaks=c(0.05,0.10, 0.5, 0.8, 1),
        text.cell=list(col="white", cex=1), max.col=170, border=NA, fmt.cell='%.2f', fmt.key='%.2f')
-  
+  print(effects)
   return(model_list)
 }
 
-get_significant_modelsf = function(num_val){
+get_significant_modelsf = function(num_val, effect_cat){
   baselines = c(-1, 0, 2)
   pvalues = matrix(NA, nrow = 4, ncol = 4)
+  effects = matrix(NA, nrow = 4, ncol = 4)
   r = 0
   model_list = list()
   for (b in baselines) {
@@ -173,17 +176,20 @@ get_significant_modelsf = function(num_val){
       c = c + 1
       e = as.integer(e) + 2 
       pvalues[r,e] = model_table[c,num_val]
+      effects[r,e] = model_table[c, effect_cat]
     } 
   }
-  colnames(pvalues) = c(-1, 0, NA, 2)
-  rownames(pvalues) = c(-1, 0, NA, 2) 
-  
+  colnames(pvalues) = c("T1 only", "neither trial", NA, "both trials")
+  rownames(pvalues) = c("T1 only", "neither trial", NA, "both trials") 
+  colnames(effects) = c("T1 only", "neither trial", NA, "both trials")
+  rownames(effects) = c("T1 only", "neither trial", NA, "both trials") 
+  #browser()
   #plot
   par(mar=c(5.1, 4.1, 3.1, 4.1))
   plot(pvalues, ylab="Baselines", xlab="Equation", main="p-values", 
        na.col="white", col=rev(grey.colors(7)), digits=4, 
        breaks=c(0.05,0.10, 0.5, 0.8, 1),
-       text.cell=list(col="white", cex=1), max.col=170, border=NA, fmt.cell='%.2f', fmt.key='%.2f')
+       text.cell=list(col="white", cex=1), max.col=170, border=NA, fmt.cell='%.2f', fmt.key='%.2f')#can repress these printing with fmt.cell=''
   
   return(model_list)
 }
