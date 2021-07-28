fef2dfa3f2c4725587b6d09b07f04bdbd58909ee
avbernat <57693577+avbernat@users.noreply.github.com>
2021-07-28 12:34:54 -0500

    Removed extraneous files.

--- avbernat_working_on/Rsrc/clean_morph_data.R
+++ avbernat_working_on/Rsrc/clean_morph_data.R
@@ -1,47 +1,40 @@
 library(zoo)
 library(lubridate)
-
-remove_torn_wings = function(d){
-  
-  d$drop <- FALSE
-  for(row in 1:nrow(d)){
-    if(length(unlist(strsplit(strsplit(paste("test ", d$notes[row], 
-                                             " test", sep=""), "torn")[[1]], "wing")))>2){
-      #browser()	
-      d$drop[row] <- TRUE
-    }
-  }
-  df <- d[d$drop==FALSE,]
-  return(df)
-}
+library(dplyr)
 
 read_morph_data = function(path) {
   
   data = read.csv(path, header=TRUE, sep=",", quote="", stringsAsFactors=FALSE)
   
   # Standardize population and host plant names of the all-morphology data
-  data$population[data$population=="Key Largo"]<-"Key_Largo"
-  data$population[data$population=="North Key Largo"]<-"North_Key_Largo"
-  data$population[data$population=="Plantation Key"]<-"Plantation_Key"
-  data$population[data$population=="Lake Wales"]<-"Lake_Wales"
   data$population[data$population=="GainesvilleGRT"]<-"Gainesville"
   data$population[data$population=="HomesteadBV"]<-"Homestead"
   data$population[data$population=="HomesteadGRT"]<-"Homestead"
+  data$population[data$population=="Ft.Myers"]<-"Ft. Myers"
+  data$population[data$population=="Key_Largo"]<-"Key Largo"
+  data$population[data$population=="Lake_Placid"]<-"Lake Placid"
+  data$population[data$population=="Lake_Wales"]<-"Lake Wales"
+  data$population[data$population=="North_Key_Largo"]<-"North Key Largo"
+  data$population[data$population=="Plantation_Key"]<-"Plantation Key"
   
-  data$pophost[data$pophost=="C. corindum"]<-"C.corindum"
-  data$pophost[data$pophost=="K. elegans"]<-"K.elegans"
-  data$pophost[data$pophost=="K. elegans "]<-"K.elegans"
+  data$pophost[data$pophost=="C.corindum"]<-"C. corindum"
+  data$pophost[data$pophost=="K.elegans"]<-"K. elegans"
   
-  # Only grab C.corindum and K.elegans host plants
-  data<-data[data$pophost=="C.corindum" | data$pophost=="K.elegans",]
+  data = data[order(-data$lat),] 
+  lat_order = c("Gainesville", "Leesburg", "Lake Wales", "Lake Placid", "Ft. Myers",  "Homestead",  "North Key Largo", "Key Largo", "Plantation Key")
+  data$population = as.factor(data$population)
+  data$population = factor(data$population,levels=lat_order)
+  
+  # Only grab C. corindum and K. elegans host plants
+  data<-data[data$pophost=="C. corindum" | data$pophost=="K. elegans",]
   data<-data[data$sex=="F" | data$sex=="M",]
-
+  
   # Sex recoding
   data$sex_binom <- -1
   data$sex_binom[data$sex=="F"] <- 1
   data$pophost_binom <- -1
-  data$pophost_binom[data$pophost=="K.elegans"] <- 1
-
+  data$pophost_binom[data$pophost=="K. elegans"] <- 1
+  
   # Datetime
   data$date <- paste(data$month, data$year, sep="/")
   data$datetime <- as.yearmon(data$date, "%B/%Y")
@@ -63,23 +56,12 @@ read_morph_data = function(path) {
   data$dates = dates
   data$month_of_year = month(dates)
   
-  # get consistent 
-  
-  # d = raw_data[is.na(raw_data$month_n),]
-  # 
-  # if (length(d) > 0) {
-  #   indices = as.numeric(rownames(raw_data[is.na(raw_data$month_n),]))
-  #   values = month.abb[as.integer(d$month)]
-  #   raw_data$month = replace(raw_data$month, indices, values)
-  # }
-  # 
-  
   cat("number of missing dates:", n_missing_dates, "\n\n")
   unique(data$datetime)
   
   # morph recording
   cat("morph types:", unique(data$w_morph), "\n")
-  cat("recoding...\nS if wing2thorax <=2.2, L if wing2thorax >=2.5\n")
+  cat("   recoding missing morph types...\n   S if wing2thorax <=2.2, L if wing2thorax >=2.5\n\n")
   
   data$wing2thorax <- data$wing/data$thorax
   
@@ -90,9 +72,28 @@ read_morph_data = function(path) {
   data$wing_morph_binom[data$w_morph=="S"]<-0
   data$wing_morph_binom[data$w_morph=="L"]<-1
   
+  ambiguous = rbind(data[data$w_morph=="LS",],data[data$w_morph=="SL",])
+  cat("ambiguous wing morph bug count: ", nrow(ambiguous), "\n\n")
+  
   # Pulling out only long wing morph and computing wing2body ratio
   data_long<-data[data$w_morph=="L",]
   data_long$wing2body <- data_long$wing/as.numeric(data_long$body)
+  cat("filtered out NA wing2body for data_long...\n")
+  
+  # Remove NA wing2body
+  data_long = data_long %>%
+    filter(!is.na(wing2body))
+  
+  # Standardize variables of interest
+  data_long$wing2body_c = (data_long$wing2body-mean(data_long$wing2body, na.rm=TRUE))
+  data_long$month_of_year_c = (data_long$month_of_year-mean(data_long$month_of_year, na.rm=TRUE))
+  data_long$months_since_start_c = (data_long$months_since_start-mean(data_long$months_since_start, na.rm=TRUE))
+  
+  # Filter out outliers
+  data_long = data_long %>% 
+    filter(!beak<4.5) %>% 
+    filter(!wing2body <0.62) %>% 
+    filter(!wing2thorax <2.2) 
   
   # Returning the data
   data_list = list(data, data_long)
