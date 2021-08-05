44cdc1e58330ee1a905d109cf143f812f2207f50
avbernat <57693577+avbernat@users.noreply.github.com>
2021-08-05 08:40:34 -0500

    Cleaning and reviewing of flight_summary.Rmd script. This includes adding Appendix details and updating source scripts.

--- avbernat_working_on/Rsrc/clean_flight_data-Fall.R
+++ avbernat_working_on/Rsrc/clean_flight_data-Fall.R
@@ -1,5 +1,5 @@
 
-clean_flight_data.Fall <- function(path, morph_data) {
+clean_flight_data.Fall <- function(path) {
   data_all = read.csv(path,header=TRUE, sep=",", stringsAsFactors=TRUE) 
   
   #ID
@@ -9,10 +9,12 @@ clean_flight_data.Fall <- function(path, morph_data) {
   data_all$flew_b<-0
   data_all$flew_b[data_all$flew=="Y"]<-1
   
+  # Sex
+  data_all$sex_c<--1
+  data_all$sex_c[data_all$sex=="F"]<-1
+  
   # Host
-  data_all$host_temp[data_all$host_plant=="K.elegans"]<- "K. elegans"
-  data_all$host_temp[data_all$host_plant=="C. corindum"]<- "C. corindum"
-  data_all$host_c[data_all$host_temp=="K. elegans"]<- 1
+  data_all$host_c[data_all$host_plant=="K. elegans"]<- 1
   data_all$host_c[data_all$host_plant=="C. corindum"]<- -1
   
   # Wing Morph
@@ -24,7 +26,7 @@ clean_flight_data.Fall <- function(path, morph_data) {
   # Yes-No Eggs on Flight Trial Day
   data_all$eggs_b<-0
   data_all$eggs_b[data_all$eggs=="Y"]<-1
- 
+  
   # Center Data
   ## Distance From Sympatric Zone
   data_all$lat_c<-data_all$latitude-mean(data_all$latitude)
@@ -33,48 +35,9 @@ clean_flight_data.Fall <- function(path, morph_data) {
   ## Average Mass
   data_all$mass_c <- data_all$mass-mean(data_all$mass, na.rm=TRUE) 
   
-  ## Prepping Data to Merge with Morphology Data
-  
-  # Missing bugs. The morph data has *more* unique IDs than the flight data (which is a good thing). 
-  # They may have just not been flown.
-  string = paste("Missing ", length(sort(unique(morph_data$ID))) - length(sort(unique(data_all$ID))), 
-        " flight bugs from morph measurements. These bugs are: ")
- 
-  mID = sort(unique(morph_data$ID)) 
-  fID = sort(unique(data_all$ID))
-  diff = setdiff(mID, fID)
-  print(string)
-  print(diff)
-  
-  # But there are also four bugs flown that did not exist in the morph data. 53, 156, 221, and 272. I removed them.
+  # But there are four bugs flown that did not exist in the morph data. 53, 156, 221, and 272. I removed them.
   data_all = data_all[data_all$ID != 53 & data_all$ID != 156 & data_all$ID != 221 & data_all$ID != 272, ]
   
-  ## Sex Inaccuracies 
-  
-  # Also needed to fix inaccurate flight trial sex identification with the proper morph sex identification:
-  data_all[data_all$ID == 68,]$sex = morph_data[morph_data$ID == 68,]$sex
-  data_all[data_all$ID == 116,]$sex = morph_data[morph_data$ID == 116,]$sex
-  data_all[data_all$ID == 118,]$sex = morph_data[morph_data$ID == 118,]$sex
-  data_all[data_all$ID == 399,]$sex = morph_data[morph_data$ID == 399,]$sex
-
-  # Sex
-  data_all$sex_c<--1
-  data_all$sex_c[data_all$sex=="F"]<-1
-  
-  ## Merging with Morph Data
-  data <- merge(data_all, morph_data, by=c("ID"))
-  
-  # fix the poor merging to match the morph_data writing | not clean code but the best way around it
-  data$sex = data$sex.y
-  data$population = data$population.y
-  data_merged = data %>%
-    select(-sex.x, -population.x, -sex.y, -population.y)
-  
-  # Check that the merge worked:
-  string = paste("Did the merge work?", nrow(data_all) == nrow(data))
-  print(string)
-  
-  data_list = list(data_all, data_merged)
-  return(data_merged)
+  return(data_all)
   
 }
