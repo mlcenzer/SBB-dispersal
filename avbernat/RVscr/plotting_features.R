f64618d9435d10343a352d29d2d11db77af3968f
avbernat <57693577+avbernat@users.noreply.github.com>
2021-12-15 09:25:36 -0600

    Leftover updates, adding interaction term figures.

---
+++ avbernat/RVscr/plotting_features.R
@@ -0,0 +1,31 @@
+## colors
+pink = "#b57783"
+darkred = "#820331"
+red = "#cd5c5c"
+grey = "#999999"
+grey = "grey78"
+#yellow = "#E69F00"
+blue = "#4255d4"
+orange = "#e34a33"
+lightorange = "#fdbb84"
+
+red = "#de2d26"
+#red = "#2ca25f"
+#red = orange
+#grey = "#99d8c9" # "grey48"
+black = "black"
+
+#lightorange = "#bdb0af"
+lightorange = orange
+orange = "#bdb0af"
+
+red = "#bdb0af"
+red = "black"
+black = "#bdb0af"
+
+
+
+red = "#de2d26"
+lightred = "#db5c5c"
+brown = "#654321"
+lightbrown = "#80635b"
